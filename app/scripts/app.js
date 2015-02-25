08182f20c6b8d1a8a91535a76c62a01bd3c7e33d
bltnico <bellot.n@gmail.com>
2015-02-25 14:55:31 +0100

    html5 mode

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,8 +17,11 @@ angular
         'ngSanitize',
         'ngTouch'
     ])
-    .config(function ($routeProvider) {
+    .config(function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
             .otherwise({ redirectTo: '/' });
+
+        if(window.history && window.history.pushState)
+            $locationProvider.html5Mode(true);
     });
