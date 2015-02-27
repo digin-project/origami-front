396047046521209493d88daf9d2789d88b222ce3
bltnico <bellot.n@gmail.com>
2015-02-27 15:39:04 +0100

    Add ngDraggable

--- app/scripts/controllers/interface.js
+++ app/scripts/controllers/interface.js
@@ -3,4 +3,9 @@
 angular.module('origami')
     .controller('InterfaceCtrl', function($scope, $http, $log) {
 
+        var iframe = angular.element('iframe')[0].contentWindow.document,
+            $body = angular.element('body', iframe);
+
+        $body.html('ok');
+        console.log($body);
     });
